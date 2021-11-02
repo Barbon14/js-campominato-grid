6b29f6eab9de6d51d0cedf8064e05fc2a5e81146
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-02 14:48:28 +0100

    aggiunto creazione quadrati in base a difficoltà

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro(o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+
+// chiedo all'utente di scegliere la difficoltà
+let difficulty = parseInt(prompt(`Seleziona il livello di difficoltà:
+1 per facile
+2 per medio
+3 per difficile`));
+console.log(difficulty);
+
+// il numero dei quadrati varia in base alla difficoltà scelta
+// - difficulty == 1 , genero 100 quadrati
+// - difficulty == 2 , genero 81 quadrati
+// - difficulty == 3 , genero 49 quadrati
+if (difficulty === 1) {
+    difficulty = 100;
+} else if (difficulty === 2) {
+    difficulty = 81;
+} else if (difficulty === 3) {
+    difficulty = 49;
+}
+console.log(difficulty);
+
+// genero in pagina i quadrati
+const squareCont = document.getElementById("container")
+for (let i = 1; i <= difficulty; i++) {
+    let node = newElement ("div", "square");
+    node.innerHTML = i;
+    squareCont.append(node);
+}
+
+// funzioni
+function newElement (newElementTag, newElementClass) {
+    let element = document.createElement(newElementTag);
+    element.classList.add(newElementClass);
+    return element;
+}
